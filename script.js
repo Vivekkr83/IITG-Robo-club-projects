d9477b5d6e7c75eb3c33657c981a7a5601f4068f
Vivekkr83 <75994974+Vivekkr83@users.noreply.github.com>
2021-10-05 00:19:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
